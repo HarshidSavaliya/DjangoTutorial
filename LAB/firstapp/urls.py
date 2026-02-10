c7d45f9797cd4131710e77cf8149455c225f0f41
Harshid Savaliya <harshidsavaliya14@gmail.com>
2026-02-10 22:23:30 +0530

    Lab7

--- LAB/firstapp/urls.py
+++ LAB/firstapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.home, name='home'),  
+    path('', views.home, name='home'), 
+    path('calculate/', views.calculate, name='calculate'),
+    path('add/', views.add, name='add'),
 ]
